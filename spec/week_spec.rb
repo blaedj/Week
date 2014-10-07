0f56c783359c5ac51130f5c3ea2dccebb2f6ec7a
Blaed <blaedj@gmail.com>
2014-10-07 15:09:25 -0500

    add days and ensure that the week starts on a monday

--- spec/week_spec.rb
+++ spec/week_spec.rb
@@ -1,12 +1,69 @@
 require './week'
 
 describe Week do
-  before do
-    @week = Week.new()
+
+
+  describe "when initialized with no params" do
+
+    it "should default to the current week" do
+      the_date = Date.new(2014, 10, 6)
+      allow(Date).to receive(:today).and_return(the_date)
+      @week = Week.new
+      expect(@week.start_date).to eql(the_date)
+    end
+
+    describe "on a day that is not monday" do
+      before do
+        the_date = Date.new(2014, 10, 7)
+        allow(Date).to receive(:today).and_return(the_date)
+      end
+
+      it "should start on a monday" do
+        @week = Week.new
+        expect(@week.start_date.monday?).to be_truthy
+      end
+
+      it "should start on the closest past monday" do
+        @week = Week.new
+        expect(@week.start_date).to eql(Date.new(2014, 10, 6))
+      end
+    end
   end
 
-  it "should start on a monday" do
-    expect(@week.start_date.monday?).to be_true
+  describe "when created with a non-monday date" do
+    before do
+      @week = Week.new(Date.new(2014, 10, 14))
+    end
+
+    it "should start on a monday" do
+      expect(@week.start_date.monday?).to be_truthy
+    end
+
+    it "should start on the closest past monday" do
+      expect(@week.start_date).to eql(Date.new(2014, 10, 13))
+    end
+  end
+
+  describe "days of the week" do
+    before do
+      @week = Week.new(Date.new(2014, 10, 14))
+    end
+
+    it "should include all the days of the week" do
+      expect(@week).to respond_to(:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday)
+    end
+
+    it "should be valid in relation to the starting date" do
+      the_date = Date.new(2014, 10, 13)
+      expect(@week.monday).to eql(the_date)
+      expect(@week.tuesday).to eql(the_date + 1)
+      expect(@week.wednesday).to eql(the_date + 2)
+      expect(@week.thursday).to eql(the_date + 3)
+      expect(@week.friday).to eql(the_date + 4)
+      expect(@week.saturday).to eql(the_date + 5)
+      expect(@week.sunday).to eql(the_date + 6)
+    end
+
   end
 
 
--- week.rb
+++ week.rb
@@ -1,9 +1,19 @@
 class Week
   # starts on a monday
-  attr_accessor :start_date
+  attr_accessor :start_date, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday
 
-  def initialize
-    @start_date = Date.new(2014, 10, 6)
+  def initialize(date = Date.today)
+    @start_date = find_monday(date)
+    process_days
+  end
+
+  def find_monday date
+    date.monday? ? date : find_monday(date - 1)
+  end
+
+  def process_days
+   days = {:monday => 0, :tuesday => 1, :wednesday => 2, :thursday => 3, :friday => 4, :saturday => 5, :sunday => 6}
+   days.each { |day, val| instance_variable_set("@#{day}", @start_date + val ) }
   end
 
 end
