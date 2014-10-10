c4cd9a0e1407f799fd1d30c2fff0a02ba0d7edfd
Blaed <blaed@anomander.local>
2014-10-10 12:18:54 -0500

    docs, add 'end_date', bump version

--- lib/sane_week.rb
+++ lib/sane_week.rb
@@ -1,21 +1,31 @@
 require 'Date'
 
+# a week that always starts on a monday
 class Week
-  # starts on a monday
-  attr_accessor :start_date, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday
+
+  attr_accessor :start_date, :end_date,
+                # get the dates for each day of the week
+                :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday
 
   def initialize(date = Date.today)
     @start_date = find_monday(date)
     process_days
   end
 
+  #gets the nearest past monday relative to the date given
   def find_monday date
     date.monday? ? date : find_monday(date - 1)
   end
 
+  def end_date
+    @sunday
+  end
+
+  private
+
   def process_days
-   days = {:monday => 0, :tuesday => 1, :wednesday => 2, :thursday => 3, :friday => 4, :saturday => 5, :sunday => 6}
-   days.each { |day, val| instance_variable_set("@#{day}", @start_date + val ) }
+    days = {:monday => 0, :tuesday => 1, :wednesday => 2, :thursday => 3, :friday => 4, :saturday => 5, :sunday => 6}
+    days.each { |day, val| instance_variable_set("@#{day}", @start_date + val ) }
   end
 
 end
--- sane_week.gemspec
+++ sane_week.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'sane_week'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.date        = '2014-10-09'
   s.summary     = "weeks starting on mondays"
   s.description = "A simple gem for weeks starting on monday"
